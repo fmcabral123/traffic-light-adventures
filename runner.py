648bd482669b51af78d3f09559b8c108a76fb197
Franco Cabral <75599698+fmcabral123@users.noreply.github.com>
2021-11-16 12:21:54 +0800

    Add files via upload

--- runner.py
+++ runner.py
@@ -27,14 +27,13 @@ def get_options():
 
 import csv
 import pandas as pd
-import plotly.express as px
 
-def run(): # call this if we want to run the adaptive algo we made
+def run(): # ignore this.
     step = 0
-    min = 37
+    min = 47
     T = 3600
     hungerlevel = [0, 0]
-    traci.trafficlight.setPhase("gneJ27", 0) # phase indexes run from 0 to n - 1 if we have n phases, like a list
+    traci.trafficlight.setPhase("gneJ27", 0)
 
     row = ["time", "waiting time", "queue length", "departure rate", "phase"]
     rows = []
@@ -42,24 +41,22 @@ def run(): # call this if we want to run the adaptive algo we made
 
     while step <= T: 
         traci.simulationStep()
-        if step % min == 0: # runs iff step mod min equiv 0, so we don't go _too_ quick
+        if step % min == 0: 
             scores = algo(hungerlevel)
-            if traci.trafficlight.getPhase("gneJ27") == 0: # everything here is now just running through the phases. the "pivot" phases are 0 and 3. the "less important" phases after 0 are 1 and 2 (yellowlights), and likewise 4 and 5 for phase 3
+            if traci.trafficlight.getPhase("gneJ27") == 0: 
                 if scores[1] > scores[0]:
                     traci.trafficlight.setPhase("gneJ27", 1)
+                    hungerlevel[0] += 5
                     hungerlevel[1] = 0
                 else:
                     traci.trafficlight.setPhase("gneJ27", 0)
-                    min = (traci.lane.getLastStepLength("gneE20_1") + traci.lane.getLastStepLength("gneE20_2") + traci.lane.getLastStepLength("gneE19_1") + traci.lane.getLastStepLength("gneE19_2")) / (4 * traci.lane.getMaxSpeed("gneE20_1"))
-                    hungerlevel[1] += 3
             elif traci.trafficlight.getPhase("gneJ27") == 3:
                 if scores[0] > scores[1]:
                     traci.trafficlight.setPhase("gneJ27", 4)
                     hungerlevel[0] = 0
+                    hungerlevel[1] += 5
                 else:
                     traci.trafficlight.setPhase("gneJ27", 3)
-                    min = (traci.lane.getLastStepLength("gneE21_1") + traci.lane.getLastStepLength("gneE21_2")) / (2 * traci.lane.getMaxSpeed("gneE21_1"))
-                    hungerlevel[0] += 3    
         waiting = traci.lane.getWaitingTime("gneE20_1") + traci.lane.getWaitingTime("gneE20_2") + traci.lane.getWaitingTime("gneE19_1") + traci.lane.getWaitingTime("gneE19_2") + traci.lane.getWaitingTime("gneE21_1") + traci.lane.getWaitingTime("gneE21_2")
         volume = traci.lane.getLastStepVehicleNumber("gneE20_1") + traci.lane.getLastStepVehicleNumber("gneE20_2") + traci.lane.getLastStepVehicleNumber("gneE19_1") + traci.lane.getLastStepVehicleNumber("gneE19_2") + traci.lane.getLastStepVehicleNumber("gneE21_1") + traci.lane.getLastStepVehicleNumber("gneE21_2")    
         exit = traci.simulation.getArrivedNumber()
@@ -69,6 +66,115 @@ def run(): # call this if we want to run the adaptive algo we made
         exited += data[3]
         rows.append(data)
         step += 1
+    
+    # "gneE20_1" + "gneE20_2" + "gneE19_1" + "gneE19_2": phase 0
+    # "gneE21_1" + "gneE21_2": phase 3
+
+    f1 = "results/dataalgo.csv" # for instantaneous values
+    f2 = "results/dataalgoave.csv" # for average values
+  
+    # writing to csv file
+    with open(f1, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        
+        csvwriter.writerow(row)
+        
+        csvwriter.writerows(rows)
+
+    df1 = pd.read_csv('results/dataalgo.csv')
+    df1.to_csv('results/dataalgo.csv', index=False)
+    
+    with open(f2, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        
+        csvwriter.writerow(["average waiting time", "average queue length", "average departure rate"])
+        csvwriter.writerow([waitingtime / T, queuelength / T, exited / T])
+
+    df2 = pd.read_csv('results/dataalgoave.csv')
+    df2.to_csv('results/dataalgoave.csv', index=False)
+
+    traci.close()
+    sys.stdout.flush()
+
+def run2(): # THIS IS THE REVISED ALGORITHM, WORK HERE! also don't forget to do run2() at the bottom
+    step = 0
+    min = 0
+    T = 3600
+    phase = -1
+    ticker = 0
+    hungerlevel = [0, 0]
+    traci.trafficlight.setPhase("gneJ27", 0)
+
+    row = ["time", "waiting time", "queue length", "departure rate", "phase"]
+    rows = []
+    waitingtime, queuelength, exited = 0, 0, 0
+
+    while step <= T: 
+        traci.simulationStep()
+        if traci.trafficlight.getPhase("gneJ27") == 0:
+            if traci.trafficlight.getPhase("gneJ27") != phase:
+                length = 1
+                velocity = 1
+                min = length / velocity # TASK 1: compute average length of vehicle queues (in m) per lane, and average velocity of vehicles (m/s) per lane. i believe there is something sa lane value retrieval that allows us to do this
+                traci.trafficlight.setPhase("gneJ27", 0)
+                ticker += 1
+            else:
+                if ticker < min:
+                    traci.trafficlight.setPhase("gneJ27", 0)
+                    ticker += 1
+                else:
+                    scores = algo(hungerlevel)
+                    ticker = 0
+                    if scores[1] > scores [0]:
+                        traci.trafficlight.setPhase("gneJ27", 1)
+                        hungerlevel[0] += 5
+                        hungerlevel[1] = 0
+                    else:
+                        length = 1
+                        velocity = 1
+                        min = length / velocity # TASK 2: copy paste code in TASK 1 HAHAHA
+                        traci.trafficlight.setPhase("gneJ27", 0)
+                        ticker += 1
+
+        elif traci.trafficlight.getPhase("gneJ27") == 3:
+            if traci.trafficlight.getPhase("gneJ27") != phase:
+                length = 1
+                velocity = 1
+                min = length / velocity # TASK 3: do the same thing in TASK 1, but now for the lanes in phase 3. note that task 1 is for lanes in phase 0.
+                traci.trafficlight.setPhase("gneJ27", 3)
+                ticker += 1
+            else:
+                if ticker < min:
+                    traci.trafficlight.setPhase("gneJ27", 3)
+                    ticker += 1
+                else:
+                    scores = algo(hungerlevel)
+                    ticker = 0
+                    if scores[0] > scores [1]:
+                        traci.trafficlight.setPhase("gneJ27", 4)
+                        hungerlevel[0] = 0
+                        hungerlevel[1] += 5
+                    else:
+                        length = 1
+                        velocity = 1
+                        min = length / velocity # TASK 4: copy paste code in TASK 3
+                        traci.trafficlight.setPhase("gneJ27", 3)
+                        ticker += 1
+
+        
+        phase = traci.trafficlight.getPhase("gneJ27")
+        waiting = traci.lane.getWaitingTime("gneE20_1") + traci.lane.getWaitingTime("gneE20_2") + traci.lane.getWaitingTime("gneE19_1") + traci.lane.getWaitingTime("gneE19_2") + traci.lane.getWaitingTime("gneE21_1") + traci.lane.getWaitingTime("gneE21_2")
+        volume = traci.lane.getLastStepVehicleNumber("gneE20_1") + traci.lane.getLastStepVehicleNumber("gneE20_2") + traci.lane.getLastStepVehicleNumber("gneE19_1") + traci.lane.getLastStepVehicleNumber("gneE19_2") + traci.lane.getLastStepVehicleNumber("gneE21_1") + traci.lane.getLastStepVehicleNumber("gneE21_2")    
+        exit = traci.simulation.getArrivedNumber()
+        data = [step, waiting / volume, volume / 6, exit, traci.trafficlight.getPhase("gneJ27")]
+        waitingtime += data[1]
+        queuelength += data[2]
+        exited += data[3]
+        rows.append(data)
+        step += 1
+    
+    # "gneE20_1" + "gneE20_2" + "gneE19_1" + "gneE19_2": phase 0
+    # "gneE21_1" + "gneE21_2": phase 3
 
     f1 = "results/dataalgo.csv" # for instantaneous values
     f2 = "results/dataalgoave.csv" # for average values
@@ -177,4 +283,4 @@ if __name__ == "__main__":
     # subprocess and then the python script connects and runs
     traci.start([sumoBinary, "-c", "data/T.sumocfg",
                              "--tripinfo-output", "tripinfo.xml"])
-    runfixed()
+    run2()
